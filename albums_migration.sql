346f5afcd54ced6ffbbc1bb067dd8cf94601a759
lo v <lov@los-MacBook-Pro.local>
2016-05-05 16:12:13 -0500

    Created a table 'albums' using MySQL

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(60),
+    name VARCHAR(100) NOT NULL,
+    release_date YEAR,
+    sales DECIMAL(10, 2),
+    genre VARCHAR(50),
+    PRIMARY KEY(id)
+
+);
