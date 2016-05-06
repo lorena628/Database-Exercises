ea8077fd70952dff0ca08ba7e7583c1e9a0dee69
lo v <lov@los-MacBook-Pro.local>
2016-05-06 13:35:50 -0500

    changed year to output 4 digits

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(60),
     name VARCHAR(100) NOT NULL,
-    release_date YEAR,
+    release_date YEAR(4),
     sales DECIMAL(10, 2),
     genre VARCHAR(50),
     PRIMARY KEY(id)
