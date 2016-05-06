4504820669d45f766c7f078ba50d057a4429f524
lo v <lov@los-MacBook-Pro.local>
2016-05-06 14:29:34 -0500

    added a comment to show before deletions and after deletions

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,5 +1,9 @@
 USE codeup_test_db;
--- SELECT "HELLO";
+
+SELECT '' AS "Table before deletes";
+
+SELECT * FROM albums;
+
 SELECT name AS 'albums released after 1991'
 FROM albums
 WHERE release_date >= 1991;
@@ -11,3 +15,14 @@ WHERE genre = "Disco";
 SELECT name AS "albums by Whitney Houston" 
 FROM albums
 WHERE artist = "Whitney Houston";
+
+
+
+-- --Now to delete :( ------
+SELECT '' AS "Table after deletes :(";
+DELETE FROM albums WHERE id = 4;
+DELETE FROM albums WHERE id = 9;
+DELETE FROM albums WHERE id = 7;
+
+-- this will show the resulting table after deletes
+SELECT * FROM albums;
