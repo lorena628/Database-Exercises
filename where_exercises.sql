a5fe295c9c095f90dc3f1a2a0bd80824269a92ae
lo v <lov@los-MacBook-Pro.local>
2016-05-07 21:13:58 -0500

    exercises using pro search queries

---
+++ where_exercises.sql
@@ -0,0 +1,20 @@
+SELECT DISTINCT first_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT * FROM employees 
+WHERE hire_date 
+BETWEEN '1990-01-01' AND '1999-12-31';
+
+
+SELECT last_name
+FROM employees
+WHERE birth_date LIKE  '%-12-25';
+
+SELECT * 
+FROM employees
+WHERE last_name LIKE '%q%';
