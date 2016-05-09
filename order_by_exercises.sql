1cda8389dcb976150fc4a424ef6f4de7c39e8c1f
lo v <lov@los-MacBook-Pro.local>
2016-05-09 14:07:46 -0500

    switched birth and hire date for last query

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -18,5 +18,5 @@ FROM employees
 WHERE hire_date 
 BETWEEN '1990-01-01' AND '1999-12-31'
     AND birth_date LIKE  '%-12-25'
-ORDER BY hire_date DESC, birth_date ASC;
+ORDER BY birth_date ASC, hire_date DESC;
     
