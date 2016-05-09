4526cd1c0f69cc245d9ec0ea3d13a0cdd9aea902
lo v <lov@los-MacBook-Pro.local>
2016-05-09 14:08:14 -0500

    completed

--- where_exercises.sql
+++ where_exercises.sql
@@ -17,7 +17,7 @@ WHERE birth_date LIKE  '%-12-25';
 SELECT * 
 FROM employees
 WHERE last_name LIKE '%q%';
-
+-- -----------------------------------
 SELECT *
 FROM employees
 WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name ='Maya';
