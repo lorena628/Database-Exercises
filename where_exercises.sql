7c50ac667c958a7f550f3303e0fc8ac72d044602
lo v <lov@los-MacBook-Pro.local>
2016-05-09 09:57:35 -0500

    added statements with chaining where clauses

--- where_exercises.sql
+++ where_exercises.sql
@@ -43,3 +43,8 @@ WHERE hire_date
 BETWEEN '1990-01-01' AND '1999-12-31'
     AND birth_date LIKE  '%-12-25';
 
+SELECT * 
+FROM employees 
+WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%';
+
